6a8460d3b62f4be220acda849d6fc8f06361f062
tokais <tobias.kaiser143@gmail.com>
2025-05-06 18:20:47 +0200

    fixed erros, made I0 callable

--- neuron.py
+++ neuron.py
@@ -10,9 +10,9 @@ class Neuron:
 
         Parameters:
             C (float, optional): Membrane capacitance in μF/cm². Default is 1.0.
-            E_Na (float, optional): Sodium (Na) reversal potential in mV. Default is 115.0.
-            E_K (float, optional): Potassium (K) reversal potential in mV. Default is -12.0.
-            E_L (float, optional): Leak reversal potential in mV. Default is 10.6.
+            E_Na (float, optional): Relative sodium (Na) reversal potential in mV. Default is 115.0.
+            E_K (float, optional): Relative potassium (K) reversal potential in mV. Default is -12.0.
+            E_L (float, optional):  Relative leak reversal potential in mV. Default is 10.6.
             g_Na (callable, optional): Sodium conductance function (mS/cm²) as a function of voltage (V).
                  If None, uses a constant conductance equal to g_Na_max. Default is None.
             g_Na_max (float, optional): Maximum sodium conductance (mS/cm²) used if g_Na is None. Default is 120.0.
@@ -27,9 +27,9 @@ class Neuron:
         self.E_Na = E_Na
         self.E_K = E_K
         self.E_L = E_L
-        self.g_Na = lambda _: g_Na_max if g_Na is None else g_Na
-        self.g_K = lambda _: g_K_max if g_K is None else g_K
-        self.g_L = lambda _: g_L_max if g_L is None else g_L
+        self.g_Na = lambda V: g_Na_max if g_Na is None else g_Na(V)
+        self.g_K = lambda V: g_K_max if g_K is None else g_K(V)
+        self.g_L = lambda V: g_L_max if g_L is None else g_L(V)
     
     def simulate(self, dt, t0, tf, V0, I0):
         """
@@ -39,8 +39,8 @@ class Neuron:
             dt (float): Time step size in milliseconds.
             t0 (float): Initial simulation time in milliseconds.
             tf (float): Final simulation time in milliseconds.
-            V0 (float): Initial membrane potential in mV.
-            I0 (float): Applied constant current in μA/cm² during the simulation.
+            V0 (float): Relative initial membrane potential in mV.
+            I0 (callable): Function returning applied current in μA/cm² for given time parameter (ms).
 
         Returns:
             tuple: A tuple (t, y) where:
@@ -48,16 +48,16 @@ class Neuron:
                 y (numpy.ndarray): Array of shape (n_steps+1, 4) containing the state variables [V, n, m, h] at each time step,
                                    where V is membrane potential (mV), and n, m, h are gating variables.
         """
-        n_inf, tau_n = self.x_inf(self.alpha_n, self.beta_n), self.tau_x(self.alpha_n, self.beta_n)
-        m_inf, tau_m = self.x_inf(self.alpha_m, self.beta_m), self.tau_x(self.alpha_m, self.beta_m)
-        h_inf, tau_h = self.x_inf(self.alpha_h, self.beta_h), self.tau_x(self.alpha_h, self.beta_h)
+        n_inf, tau_n = self.inf_and_tau(self.alpha_n, self.beta_n)
+        m_inf, tau_m = self.inf_and_tau(self.alpha_m, self.beta_m)
+        h_inf, tau_h = self.inf_and_tau(self.alpha_h, self.beta_h)
         
-        def hodgkin_huxley(_, y):
+        def hodgkin_huxley(t, y):
             V, n, m, h = y
             dVdt = ((n**4 * self.g_K(V) * (self.E_K - V)) +
                     (m**3 * h * self.g_Na(V) * (self.E_Na - V)) +
                     (self.g_L(V) * (self.E_L - V)) +
-                    I0) / self.C
+                    I0(t)) / self.C
             dndt = (n_inf(V) - n) / tau_n(V)
             dmdt = (m_inf(V) - m) / tau_m(V)
             dhdt = (h_inf(V) - h) / tau_h(V)
@@ -67,11 +67,10 @@ class Neuron:
         t, y = self._rk4(hodgkin_huxley, t0, y0, dt, int((tf-t0)/dt))
         return t, y
         
-    def x_inf(self, alpha, beta): 
-        return lambda V: alpha(V) / (alpha(V) + beta(V))
-    
-    def tau_x(self, alpha, beta): 
-        return lambda V: 1 / (alpha(V) + beta(V))
+    def inf_and_tau(self, alpha, beta):
+        x_inf = lambda V: alpha(V) / (alpha(V) + beta(V))
+        tau_x = lambda V: 1. / (alpha(V) + beta(V))
+        return x_inf, tau_x
     
     def alpha_n(self, V):
         return (0.01*(10-V)) / (np.exp((10-V)/10)-1)
@@ -103,7 +102,7 @@ class Neuron:
             k3 = np.array(f(t + dt/2, y + (dt * k2)/2))
             k4 = np.array(f(t + dt, y + dt * k3))
 
-            y += (1./6) * dt * (k1 + 2*k2 + 2*k3 + k4)
+            y += dt * (k1 + 2*k2 + 2*k3 + k4) / 6
             t += dt
             
             ts.append(t)
@@ -114,11 +113,20 @@ class Neuron:
 
 def main():
     neuron = Neuron()
-    t, y = neuron.simulate(.05, 0., 50., -50., 10.)
-    plt.plot(t, y[:,0])
-    plt.xlabel('Time (ms)')
-    plt.ylabel('Membrane Potential (mV)')
-    plt.title('Hodgkin-Huxley Simulation')
+    t, y = neuron.simulate(.05, 0., 650., 0., lambda t: 0. if (t%200) < 100 else 40.)
+    _, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(10, 8))
+    ax1.plot(t, y[:, 0])
+    ax1.set_ylabel('Membrane Potential (mV)')
+    ax1.set_title('Hodgkin-Huxley Neuron Dynamics')
+    ax1.grid(True)
+    ax2.plot(t, y[:, 1], label='n')
+    ax2.plot(t, y[:, 2], label='m')
+    ax2.plot(t, y[:, 3], label='h')
+    ax2.set_xlabel('Time (ms)')
+    ax2.set_ylabel('Gating Variable Value')
+    ax2.legend()
+    ax2.grid(True)
+    plt.tight_layout()
     plt.show()
 
 
